91f7a1bc2e73359eb93c122b16321aee9e74076b
Oleg Silkin <o.silkin98@gmail.com>
2021-04-13 22:14:15 -0400

    added gitignore

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <title>title</title>
   </head>
   <body>
-  
+
+    <form action="processing.php" method="post">
+    Artist Name: <input type="text" name="artist_name"><br>
+    <input type="submit">
+    </form>
   </body>
 </html>
